93465b47f4b8ab1c53069b953fc8bd1c53180b37
ParosK <paroskwan@gmail.com>
2017-01-18 22:24:11 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -88,7 +88,7 @@ public class MainActivity extends android.app.Activity {
         });
     }
 
-
+//thing below are auto generated 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
